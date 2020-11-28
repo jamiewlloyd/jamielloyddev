6c066e43c28d35d4be5c4de5e605bf237b1557c8
Jamie Lloyd <jamielloyd.work@gmail.com>
2020-11-28 19:39:32 +0000

    Test for mobile devices
    testing view height javascript for mobile

--- js/script.js
+++ js/script.js
@@ -15,3 +15,8 @@ function nextContainer(event) {
 function backToTop() {
     header.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
+
+//Get the viewport height and multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
